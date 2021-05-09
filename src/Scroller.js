1b5358476a5aefaca22a75c7638fe43d5e36e4aa
Twiddly <pew@pewpew.moe>
2021-05-09 16:11:54 +0200

    Update Scroller.js
    add Option to Center Content to Client when the Content is smaller then the Client

--- src/Scroller.js
+++ src/Scroller.js
@@ -27,6 +27,9 @@ export default class {
 
       /** Enable scrolling on y-axis */
       scrollingY: true,
+      
+      /** Enable content to center in client when zooming out */
+      centering: false,
 
       /** Enable animations for deceleration, snap back, zooming and scrolling */
       animating: true,
@@ -180,6 +183,12 @@ export default class {
 
     /** {Number} Scheduled zoom level (final scale when animating) */
     this.__scheduledZoom = 0;
+    
+    /* {Number} Offset to center left position */
+    this.__leftOffset = 0;
+
+    /* {Number} Offset to center top position */
+    this.__topOffset = 0;
 
     /**
      ---------------------------------------------------------------------------
@@ -334,8 +343,8 @@ export default class {
    */
   getValues() {
     return {
-      left: this.__scrollLeft,
-      top: this.__scrollTop,
+      left: this.__scrollLeft - (this.options.centering ? this.__leftOffset : 0),
+      top: this.__scrollTop - (this.options.centering ? this.__topOffset : 0),
       zoom: this.__zoomLevel
     };
   }
@@ -924,7 +933,7 @@ export default class {
 
           // Push values out
           if (this.__callback) {
-            this.__callback(this.__scrollLeft, this.__scrollTop, this.__zoomLevel);
+            this.__callback(this.__scrollLeft - this.__leftOffset, this.__scrollTop - this.__topOffset, this.__zoomLevel);
           }
         }
       };
@@ -963,7 +972,7 @@ export default class {
 
       // Push values out
       if (this.__callback) {
-        this.__callback(left, top, zoom);
+        self.__callback(left - this.__leftOffset, top - this.__topOffset, zoom);
       }
 
       // Fix max scroll ranges
@@ -987,6 +996,27 @@ export default class {
 
     this.__maxScrollLeft = Math.max(this.__contentWidth * zoomLevel - this.__clientWidth, 0);
     this.__maxScrollTop = Math.max(this.__contentHeight * zoomLevel - this.__clientHeight, 0);
+    
+    let xoffset = 0;
+    let yoffset = 0;
+
+    if(this.options.centering){
+
+        if (this.__clientWidth > (this.__contentWidth*this.__zoomLevel)){
+            xoffset = (this.__clientWidth-(this.__contentWidth*this.__zoomLevel))/2;
+        } else {
+            xoffset = 0;
+        }
+
+        if (this.__clientHeight > (this.__contentHeight*this.__zoomLevel)){
+            yoffset = (this.__clientHeight-(this.__contentHeight*this.__zoomLevel))/2;
+        } else {
+            yoffset = 0;
+        }
+    }
+
+    this.__leftOffset = xoffset;
+    this.__topOffset = yoffset;
   }
 
   /**
